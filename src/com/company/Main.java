6eba8dc8b77f72e9d326f6974a5e1561dee85f32
gcucatti <gcucatti@dc.exa.unrc.edu.ar>
2018-09-23 20:24:00 -0300

    suma al resultado

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	    System.out.println("Hola");
 	    int result = 1+1;
-	    System.out.println(result);
+	    System.out.println(result+1);
 	    int a =1;
     }
 }
